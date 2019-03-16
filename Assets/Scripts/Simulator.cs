ae2fa374e39e7da7b006ca02f1a66daba4080c81
Alex Poolton <alexpoolton@gmail.com>
2019-03-16 22:14:04 +0000

    Added isPlaced condition to wrap all other method calls excluding Placed in simulator

--- Assets/Scripts/Simulator.cs
+++ Assets/Scripts/Simulator.cs
@@ -47,43 +47,41 @@ public class Simulator : MonoBehaviour
 
     public void RunCommand(string methodName, string[] args)
     {
-        switch(methodName)
-        {
-            case "PLACE":
-                int x = int.Parse(args[0]);
-                int y = int.Parse(args[1]);
+       if(methodName == "PLACE")
+       {
+            int x = int.Parse(args[0]);
+            int y = int.Parse(args[1]);
 
-                Robot.Direction direction = (Robot.Direction)System.Enum.Parse(typeof(Robot.Direction), args[2], true);
+            Robot.Direction direction = (Robot.Direction)System.Enum.Parse(typeof(Robot.Direction), args[2], true);
 
-                if(m_table.IsValidCell(x, y))
+            if(m_table.IsValidCell(x, y))
+            {
+                m_robot.Place(m_table.GetCell(x, y), direction);
+            }
+       }
+       else if(m_robot.IsPlaced)
+       {
+            if(methodName == "MOVE")
+            {
+                Table.Cell neighbouringCell = m_table.GetCellInDirection(m_robot.CurrentCell, m_robot.CurrentlyFacing);
+
+                if(m_table.IsValidCell(neighbouringCell.X, neighbouringCell.Y))
                 {
-                    m_robot.Place(m_table.GetCell(x, y), direction);
-                }
-            break;
-
-            case "MOVE":
-                if(m_robot.IsPlaced)
-                {
-                    Table.Cell neighbouringCell = m_table.GetCellInDirection(m_robot.CurrentCell, m_robot.CurrentlyFacing);
-
-                    if(m_table.IsValidCell(neighbouringCell.X, neighbouringCell.Y))
-                    {
-                        m_robot.CurrentCell = neighbouringCell; 
-                    }
-                }
-            break;
-
-            case "LEFT":
+                    m_robot.CurrentCell = neighbouringCell; 
+                }         
+            }
+            else if(methodName == "LEFT")
+            {
                 m_robot.Left();
-            break;
-
-            case "RIGHT":
+            }
+            else if(methodName == "RIGHT")
+            {
                 m_robot.Right();
-            break;
-
-            case "REPORT":
-                m_robot.Report();
-            break;
-        }
+            }
+            else if(methodName == "REPORT")
+            {
+                 m_robot.Report();
+            }
+       }
     }
 }
