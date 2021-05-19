462fadcdae29d8b1e91c7344ed9b8371e729cef0
Alex Poolton <alexander.poolton@mottmac.com>
2021-05-19 21:56:14 +0100

    Refactored and cleaned up simulator class

--- Assets/Scripts/Simulator.cs
+++ Assets/Scripts/Simulator.cs
@@ -14,16 +14,12 @@ public class Simulator : MonoBehaviour
     private Robot m_robot = null;
 
     [SerializeField]
-    TextAsset m_commandText = null;
+    private TextAsset m_commandText = null;
 
     public struct Command
     {
         public string Name;
         public Dictionary<string, string> Arguments;
-
-        public Command(string name) : this (name, new Dictionary<string, string>())
-        {
-        }
 
         public Command(string name, Dictionary<string, string> args)
         {
@@ -35,7 +31,7 @@ public class Simulator : MonoBehaviour
     /// <summary>
     /// Generates a table and runs the commands found in the command text file.
     /// </summary>
-    public void Start()
+    private void Start()
     {
         m_table.Generate(5, 5);
 
@@ -50,26 +46,26 @@ public class Simulator : MonoBehaviour
     /// <param name="table">The table in the simulation.</param>
     public void RunCommands(string commandText, Robot robot, Table table)
     {
-        List<Command> commands = new List<Command>();
+        var commands = new List<Command>();
 
         // Commands are split by line (as formatted in the text file).
-        string[] commandLines = commandText.Split(new string[] {"\n"}, System.StringSplitOptions.None);
+        var commandLines = commandText.Split(new string[] {"\n"}, System.StringSplitOptions.None);
 
         // Iterate through each command and split it into the method name and an arguments provided
-        foreach(string line in commandLines)
+        foreach(var line in commandLines)
         {
-            string[] command = line.Split();
+            var command = line.Split();
 
-            string name = "";
-            Dictionary<string, string> args = new Dictionary<string, string>();
+            var commandName = "";
+            var args = new Dictionary<string, string>();
 
             if(command.Length > 0)
             {
-               name = command[0];
+               commandName = command[0];
 
-                if (name == "PLACE" && command.Length > 1)
+                if (commandName == "PLACE" && command.Length > 1)
                {
-                    string[] argsRawText = command[1].Split(',');
+                    var argsRawText = command[1].Split(',');
 
                     if(argsRawText.Length > 2)
                     {
@@ -83,9 +79,9 @@ public class Simulator : MonoBehaviour
                }
             }
 
-            if(!string.IsNullOrEmpty(name))
+            if(!string.IsNullOrEmpty(commandName))
             {
-                commands.Add(new Command(name, args));
+                commands.Add(new Command(commandName, args));
             }
         }
 
@@ -94,25 +90,24 @@ public class Simulator : MonoBehaviour
 
     public IEnumerator RunCommands(List<Command> commands, Robot robot, Table table)
     {
-        foreach(Command c in commands)
+        foreach(var command in commands)
         {
-            yield return StartCoroutine(RunCommand(c, robot, table));
+            yield return StartCoroutine(RunCommand(command, robot, table));
         }
     }
 
     /// <summary>
     /// Runs a provided command.
     /// </summary>
-    /// <param name="methodName">The name of the method.</param>
-    /// <param name="args">The arguments with the provided method.</param>
+    /// <param name="command">The command to be ran</param>
     /// <param name="robot">The robot in the simulation.</param>
     /// <param name="table">The table in the simulation.</param>
     public IEnumerator RunCommand(Command command, Robot robot, Table table)
     {
         if(command.Name == "PLACE")
         {
-            int.TryParse(command.Arguments["X"], out int x);
-            int.TryParse(command.Arguments["Y"], out int y);
+            int.TryParse(command.Arguments["X"], out var x);
+            int.TryParse(command.Arguments["Y"], out var y);
             System.Enum.TryParse("FACING", true, out Robot.Facing direction);
 
             if (table.IsValidCell(x, y))
@@ -121,7 +116,7 @@ public class Simulator : MonoBehaviour
             }
             else
             {
-                Debug.LogError("Tried to place robot at " + x + "," + y + " which is invalid");
+                Debug.LogError($"Tried to place robot at {x},{y} which is invalid");
             }
         }
         else if(robot.IsPlaced)
